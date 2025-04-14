7416c4b2d605c9edc48abd1638db44ee3db91e32
Shubham Sharma <techies@crobstacle.com>
2025-04-14 15:34:01 +0530

    New Content & Bg

--- src/components/Maintenance.tsx
+++ src/components/Maintenance.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { MapPin } from "lucide-react"; 
 import Image from "next/image";
+import Link from "next/link";
 
 const Maintenance = () => {
   return (
@@ -10,19 +11,21 @@ const Maintenance = () => {
       {/* Fullscreen Section */}
       <div className="flex flex-col justify-center items-center w-full h-full bg-[#000000a9] bg-opacity-50 text-white text-center">
         {/* Heading and Sub-Text */}
-        <h1 className="text-4xl md:text-6xl font-bold mb-8">
-          Your Heading Here
+        <div className="mx-auto max-w-screen-xl">
+        <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold mb-8">
+        Welcome to GOLD TOUCH THE LUXURY SPA
         </h1>
         <p className="text-lg md:text-xl mb-20">
-          Your subtext or description goes here. Add a call to action or an informative message.
+        Indulge in an unforgettable experience where relaxation meets luxury. At GOLD TOUCH THE LUXURY SPA, we offer a sanctuary of tranquility designed to rejuvenate your mind, body, and soul.
         </p>
+        </div>
         
 
       {/* Address Field with Icon */}
       <div className="px-6 md:px-12 max-w-5xl mx-auto text-lg absolute bottom-4 left-4">
           <div className="flex items-center justify-center space-x-3 mb-6">
             <MapPin size={24} className="text-white" />
-            <p className="text-base">Ground Floor, Survey No 167, 3,4, Calangute - Anjuna Rd, Naika Waddo, Calangute, Goa 403516</p>
+            <Link className="underline underline-offset-4" href="https://maps.app.goo.gl/DGbDJ8jBUSmCWzQTA"><p className="text-base">Ground Floor, Survey No 167, 3,4, Calangute - Anjuna Rd, Naika Waddo, Calangute, Goa 403516</p></Link>
           </div>
         </div>
 
